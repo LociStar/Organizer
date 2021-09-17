6bbc2fb11640a8df1a88bacae7187adadec39edd
LokiStar <locistar.got@gmail.com>
2021-09-17 12:39:19 +0200

    add guild commands if bot gets added to a new server

--- src/main/java/com/locibot/locibot/listener/GuildCreateListener.java
+++ src/main/java/com/locibot/locibot/listener/GuildCreateListener.java
@@ -1,13 +1,21 @@
 package com.locibot.locibot.listener;
 
+import com.locibot.locibot.core.command.CommandManager;
 import com.locibot.locibot.data.Telemetry;
 import discord4j.common.util.Snowflake;
+import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.guild.GuildCreateEvent;
 import reactor.core.publisher.Mono;
 
 import static com.locibot.locibot.LociBot.DEFAULT_LOGGER;
 
 public class GuildCreateListener implements EventListener<GuildCreateEvent> {
+
+    private final DiscordClient client;
+
+    public GuildCreateListener(DiscordClient client) {
+        this.client = client;
+    }
 
     @Override
     public Class<GuildCreateEvent> getEventType() {
@@ -23,7 +31,8 @@ public class GuildCreateListener implements EventListener<GuildCreateEvent> {
             final int memberCount = event.getGuild().getMemberCount();
             DEFAULT_LOGGER.debug("{Guild ID: {}} Connected ({} users)", guildId.asString(), memberCount);
         }
-        return Mono.empty();
+        DEFAULT_LOGGER.info("Registering commands");
+        return event.getClient().getApplicationInfo().flatMap(applicationInfo -> CommandManager.registerGuildCommands(client.getApplicationService(), applicationInfo.getId().asLong(), event.getGuild().getId().asLong()));
     }
 
 }
