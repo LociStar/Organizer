fc90a6372a48c9ac576fb7731a38e6efd9defaa6
LociStar <locistar.got@gmail.com>
2022-01-10 18:35:47 +0100

    locations do now get saved in DB

--- WebAPI/src/main/java/com/locibot/webapi/weatherData/WeatherDataHandler.java
+++ WebAPI/src/main/java/com/locibot/webapi/weatherData/WeatherDataHandler.java
@@ -2,6 +2,8 @@ package com.locibot.webapi.weatherData;
 
 import com.locibot.locibot.data.credential.Credential;
 import com.locibot.locibot.data.credential.CredentialManager;
+import com.locibot.locibot.database.DatabaseManager;
+import com.locibot.locibot.database.locations.entity.DBLocation;
 import com.locibot.locibot.object.RequestHelper;
 import com.locibot.locibot.utils.JWT.TokenVerification;
 import com.locibot.webapi.weatherData.ptv.PTVResponse;
@@ -25,16 +27,28 @@ public class WeatherDataHandler {
 
         try {
             if (tokenVerification.verify(Objects.requireNonNull(CredentialManager.get(Credential.JWT_SECRET)))) {
+                return DatabaseManager.getLocations().getLocation(city).flatMap(dbLocation -> {
+                    System.out.println("Test");
+                    //no Location found
+                    if (dbLocation.getBean().getLatitude() == 0) {
+                        System.out.println("Nicht gefunden");
+                        return RequestHelper.fromUrl("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city + "&apiKey=" + CredentialManager.get(Credential.PTV))
+                                .addHeaders("searchText", city)
+                                .addHeaders("apiKey", CredentialManager.get(Credential.PTV))
+                                .to(PTVResponse.class).flatMap(ptvResponse -> { //TODO: needs to be saved in a Database
+                                    Double latitude = ptvResponse.result().get(0).referencePosition().get("latitude");
+                                    Double longitude = ptvResponse.result().get(0).referencePosition().get("longitude");
+                                    DBLocation dbLocationNew = new DBLocation(city, longitude, latitude);
+                                    return dbLocationNew.insert().then(ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                                            .body(BodyInserters.fromValue(new WeatherData(latitude, longitude))));
+                                });
+                    }
+                    //location found
+                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                            .body(BodyInserters.fromValue(new WeatherData(dbLocation.getBean().getLongitude(), dbLocation.getBean().getLatitude())));
+                });
 
-                return RequestHelper.fromUrl("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city + "&apiKey=" + CredentialManager.get(Credential.PTV))
-                        .addHeaders("searchText", city)
-                        .addHeaders("apiKey", CredentialManager.get(Credential.PTV))
-                        .to(PTVResponse.class).flatMap(ptvResponse -> { //TODO: needs to be saved in a Database
-                            Double latitude = ptvResponse.result().get(0).referencePosition().get("latitude");
-                            Double longitude = ptvResponse.result().get(0).referencePosition().get("longitude");
-                            return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-                                    .body(BodyInserters.fromValue(new WeatherData(latitude, longitude)));
-                        });
+
             }
         } catch (Exception e) {
             return ServerResponse.badRequest().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(new WeatherError("bad token")));
