6c600a2861ca5f0081d8d66dfdcd4e7f30a614be
LociStar <locistar.got@gmail.com>
2022-01-27 17:34:28 +0100

    cleanup

--- WebAPI/src/main/java/com/locibot/webapi/weatherData/WeatherDataHandler.java
+++ WebAPI/src/main/java/com/locibot/webapi/weatherData/WeatherDataHandler.java
@@ -1,21 +1,9 @@
 package com.locibot.webapi.weatherData;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
-import com.github.prominence.openweathermap.api.enums.Language;
-import com.github.prominence.openweathermap.api.enums.OneCallResultOptions;
-import com.github.prominence.openweathermap.api.enums.UnitSystem;
-import com.github.prominence.openweathermap.api.model.Coordinate;
-import com.github.prominence.openweathermap.api.model.onecall.current.CurrentWeatherData;
 import com.locibot.locibot.data.credential.Credential;
 import com.locibot.locibot.data.credential.CredentialManager;
-import com.locibot.locibot.database.locations.entity.DBLocation;
 import com.locibot.locibot.utils.JWT.TokenVerification;
 import com.locibot.locibot.utils.weather.WeatherManager;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
