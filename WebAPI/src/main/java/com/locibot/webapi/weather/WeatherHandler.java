42ef4155997deee9c14ec20c8082c4cf3b262b4b
LociStar <locistar.got@gmail.com>
2022-03-07 18:07:32 +0100

    removed content since it is deprecated

--- WebAPI/src/main/java/com/locibot/webapi/weather/WeatherHandler.java
+++ WebAPI/src/main/java/com/locibot/webapi/weather/WeatherHandler.java
@@ -1,33 +1,15 @@
 package com.locibot.webapi.weather;
 
-import com.locibot.locibot.data.credential.Credential;
-import com.locibot.locibot.data.credential.CredentialManager;
-import com.locibot.locibot.utils.JWT.TokenVerification;
-import com.locibot.locibot.utils.weather.HourlyWeatherForecastClass;
-import net.aksingh.owmjapis.core.OWM;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 
 @Component
 public class WeatherHandler {
 
     public Mono<ServerResponse> weather(ServerRequest request) {
-        String city = request.queryParam("city").orElse("invalid city");
+        /*String city = request.queryParam("city").orElse("invalid city");
         String token = request.queryParam("token").orElse("invalid token");
         TokenVerification tokenVerification = new TokenVerification(token);
         try {
@@ -46,7 +28,7 @@ public class WeatherHandler {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
         return ServerResponse.badRequest().build();
 
     }
