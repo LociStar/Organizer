c27bcbaa09699e79e4080a045743e418a4a50d50
LociStar <locistar.got@gmail.com>
2022-04-22 17:49:13 +0200

    test removed

--- src/main/java/com/locibot/locibot/command/fun/Hello.java
+++ src/main/java/com/locibot/locibot/command/fun/Hello.java
@@ -2,12 +2,7 @@ package com.locibot.locibot.command.fun;
 
 import com.locibot.locibot.core.command.*;
 import discord4j.core.object.command.ApplicationCommandOption;
-import discord4j.core.spec.EmbedCreateFields;
-import discord4j.core.spec.EmbedCreateSpec;
-import discord4j.rest.util.Color;
 import reactor.core.publisher.Mono;
-
-import java.time.Instant;
 
 @CmdAnnotation
 public class Hello extends BaseCmd {
@@ -18,18 +13,6 @@ public class Hello extends BaseCmd {
     @Override
     public Mono<?> execute(Context context) {
         context.getClient();
-        return context.getEvent().getInteractionResponse().createFollowupMessage("Hello").then(
-                context.createFollowupMessage(EmbedCreateSpec.builder()
-                        .color(Color.BLUE)
-                        .title("Event invitation")
-                        .description("Invitation to **" + "test" + "**")
-                        .footer(EmbedCreateFields.Footer.of(context.localize("event.util.footer").formatted(context.getAuthor().getUsername()), context.getAuthor().getAvatarUrl()))
-                        .addField(EmbedCreateFields.Field.of(context.localize("event.description"), "empty" + "\n", false))
-                        .addField(EmbedCreateFields.Field.of(context.localize("event.util.date"),
-                                "<t:" + Instant.now().getEpochSecond() + ">",
-                                false))
-                        .addField(EmbedCreateFields.Field.of(context.localize("event.util.users"), "Test" + "\n", false))
-                        .build())
-        );
+        return context.getEvent().getInteractionResponse().createFollowupMessage("Hello");
     }
 }
