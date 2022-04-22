edfeecdff1b40263fc6a070165cd3d70ed6138ae
LociStar <locistar.got@gmail.com>
2022-04-22 17:45:00 +0200

    new Annotations

---
+++ src/main/java/com/locibot/locibot/core/command/ButtonAnnotation.java
@@ -0,0 +1,8 @@
+package com.locibot.locibot.core.command;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface ButtonAnnotation {
+}
---
+++ src/main/java/com/locibot/locibot/core/command/CmdAnnotation.java
@@ -0,0 +1,9 @@
+package com.locibot.locibot.core.command;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface CmdAnnotation {
+
+}
