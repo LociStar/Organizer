238fce22ecc65b2de772cdc5a5f6fd1529f214c8
LociStar <locistar.got@gmail.com>
2022-05-12 08:11:41 +0200

    EventReminderTask fixed
    - scheduled every minute
    - uses now the enhanced events db

--- src/main/java/com/locibot/locibot/service/tasks/EventReminderTask.java
+++ src/main/java/com/locibot/locibot/service/tasks/EventReminderTask.java
@@ -33,34 +33,35 @@ public class EventReminderTask implements Task {
     @Override
     public Disposable schedule(Scheduler scheduler) {
         LOGGER.info("Scheduling event reminder");
-        return Flux.interval(Duration.ofMinutes(5), Duration.ofMinutes(5), scheduler)
+        return Flux.interval(Duration.ofMinutes(1), Duration.ofMinutes(1), scheduler)
                 .flatMap(__ -> {
-                            LOGGER.info("Starting group reminder");
-                            DatabaseManager.getEvents().getAllEvents().forEach(dbEvent -> {
-                                Long epochSeconds = dbEvent.getBean().getScheduledDate();
-                                //check if group is scheduled
-                                if (epochSeconds != null) {
-                                    ZoneId zoneId = ZoneId.of("Europe/Berlin");
-                                    ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(epochSeconds), zoneId);
-                                    //check if date_now == group scheduled date
-                                    if (zonedDateTime.toLocalDate().isEqual(LocalDate.now())
-                                            //send invite to all members of group, if (time_now + 35min) > scheduled_time > (time_now + 25min)
-                                            && zonedDateTime.toLocalTime().isAfter(LocalTime.now().plus(1950, ChronoUnit.SECONDS))
-                                            && zonedDateTime.toLocalTime().isBefore(LocalTime.now().plus(1650, ChronoUnit.SECONDS))) {
-                                        LOGGER.info("30min reminder for group " + dbEvent.getEventName());
-                                        dbEvent.getMembers().forEach(member -> {
-                                            if (member.getBean().getAccepted() == 1)
-                                                gateway.getUserById(member.getId()).flatMap(user ->
+                    LOGGER.info("Starting group reminder");
+                    DatabaseManager.getEvents().getAllEvents().forEach(dbEvent -> {
+                        Long epochSeconds = dbEvent.getBean().getScheduledDate();
+                        //check if group is scheduled
+                        if (epochSeconds != null) {
+                            ZoneId zoneId = ZoneId.of("Europe/Berlin");
+                            ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(epochSeconds), zoneId);
+                            //check if date_now == group scheduled date
+                            if (zonedDateTime.toLocalDate().isEqual(LocalDate.now())
+                                    //send invite to all members of group, if (time_now + 35min) > scheduled_time > (time_now + 25min)
+                                    && zonedDateTime.toLocalTime().isAfter(LocalTime.now().plus(1740, ChronoUnit.SECONDS))
+                                    && zonedDateTime.toLocalTime().isBefore(LocalTime.now().plus(1830, ChronoUnit.SECONDS))) {
+                                LOGGER.info("30min reminder for group " + dbEvent.getEventName());
+                                dbEvent.getMembers().forEach(member -> {
+                                    if (member.getBean().getAccepted() == 1)
+                                        gateway.getUserById(member.getUId()).flatMap(user ->
+                                                gateway.getUserById(dbEvent.getOwner().getUId()).flatMap(owner ->
                                                         user.getPrivateChannel().flatMap(privateChannel ->
                                                                 privateChannel.createMessage(EmbedCreateSpec.builder()
                                                                         .color(Color.BLUE)
                                                                         .title("Event Reminder")
-                                                                        .description("The event: " + dbEvent.getEventName() + "will start in 30 minutes")
+                                                                        .description("The event **" + dbEvent.getEventName() + "** will start in 30 minutes")
                                                                         .addField(EmbedCreateFields.Field.of("Description", dbEvent.getEventDescription(), false))
                                                                         .thumbnail(dbEvent.getIcon())
-                                                                        .footer(EmbedCreateFields.Footer.of("Author: " + dbEvent.getOwner().getEventName(), ""))
+                                                                        .footer(EmbedCreateFields.Footer.of("Author: " + owner.getUsername(), owner.getAvatarUrl()))
                                                                         .build())
-                                                        )).subscribe();
+                                                        ))).subscribe();
                                         });
                                     }
                                 }
