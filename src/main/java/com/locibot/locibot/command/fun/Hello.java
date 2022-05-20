8c31adbc8c9e2c3cdef1448c96632c17ff356ce3
LociStar <locistar.got@gmail.com>
2022-05-20 14:45:22 +0200

    removed useless function call

--- src/main/java/com/locibot/locibot/command/fun/Hello.java
+++ src/main/java/com/locibot/locibot/command/fun/Hello.java
@@ -12,7 +12,6 @@ public class Hello extends BaseCmd {
 
     @Override
     public Mono<?> execute(Context context) {
-        context.getClient();
         return context.getEvent().getInteractionResponse().createFollowupMessage("Hello");
     }
 }
