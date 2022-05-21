70a4ae46558834445290bb2307bb918e928b6516
LociStar <locistar.got@gmail.com>
2022-05-21 16:16:14 +0200

    code cleanup

--- src/main/java/com/locibot/locibot/database/events_db/EventsCollection.java
+++ src/main/java/com/locibot/locibot/database/events_db/EventsCollection.java
@@ -34,12 +34,13 @@ public class EventsCollection extends DatabaseCollection { //TODO: replace conso
         this.eventCache = MultiValueCache.Builder.<ObjectId, DBEvent>builder().withInfiniteTtl().build();
     }
 
-    public boolean containsEvent(ObjectId eventName) {
+    @Deprecated
+    public boolean containsEvent(ObjectId objectId) {
         List<Document> documents = Flux.from(this.getCollection().find()).collectList().block();
         if (documents != null) {
             for (Document document : documents) {
                 try {
-                    if (NetUtil.MAPPER.readValue(document.toJson(JSON_WRITER_SETTINGS), DBEventBean.class).getId().equals(eventName))
+                    if (NetUtil.MAPPER.readValue(document.toJson(JSON_WRITER_SETTINGS), DBEventBean.class).getId().equals(objectId))
                         return true;
                 } catch (JsonProcessingException e) {
                     LOGGER.error(e.getMessage());
@@ -86,10 +87,6 @@ public class EventsCollection extends DatabaseCollection { //TODO: replace conso
                     .first();
         });
 
-//        final Publisher<Document> request = this.getCollection()
-//                .find(Filters.eq("_id", eventId.block()))
-//                .first();
-
         final Mono<DBEvent> getDBEvent = request
                 .flatMap(documentPublisher -> Mono.from(documentPublisher)
                         .map(document -> document.toJson(JSON_WRITER_SETTINGS))
@@ -108,55 +105,6 @@ public class EventsCollection extends DatabaseCollection { //TODO: replace conso
         return this.eventCache.getOrCache(id.get(), getDBEvent);
     }
 
-    /*public Mono<DBEvent> getDBEvent(Long id) {
-        Publisher<Document> document = this.getCollection().find(Filters.eq("_id", id)).first();
-        return Mono.from(document)
-                .map(document1 -> document1.toJson(JSON_WRITER_SETTINGS))
-                .flatMap(json -> Mono.fromCallable(() -> NetUtil.MAPPER.readValue(json, DBEventBean.class)))
-                .map(DBEvent::new);
-    }
-
-    public Mono<DBEvent> getDBEvent(Snowflake uId, String eventName) {
-
-        final Mono<List<Long>> eventsRequest = DatabaseManager.getUsers().getDBUser(uId).map(dbUser ->
-                dbUser.getBean().getEvents());
-
-        final Mono<Long> eventId = eventsRequest.flatMap(longs -> {
-            Mono<Mono<Long>> event = longs.stream().map(aLong ->
-                    this.getDBEvent(aLong).map(dbEvent ->
-                            dbEvent.getEventName().equals(eventName)).map(aBoolean -> {
-                        if (aBoolean) {
-                            return this.getDBEvent(aLong).map(DBEvent::getId);
-                        } else
-                            return Mono.just(0L);
-                    })).collect(Collectors.toList()).get(0);
-
-            return event.flatMap(longMono -> longMono);
-        });
-
-        final Mono<?> request = eventId.map(id -> this.getCollection()
-                .find(Filters.eq("_id", id))
-                .first());
-
-        final Mono<DBEvent> getDBEvent = Mono.from(request)
-                .map(o -> (Document) o)
-                .map(document -> document.toJson(JSON_WRITER_SETTINGS))
-                .flatMap(json -> Mono.fromCallable(() -> NetUtil.MAPPER.readValue(json, DBEventBean.class)))
-                .map(DBEvent::new)
-                .doOnSuccess(consumer -> {
-                    if (consumer == null) {
-                        LOGGER.debug("[DBEvent {}] Not found", eventName);
-                    }
-                })
-                .defaultIfEmpty(new DBEvent(eventName))
-                .doOnSubscribe(__ -> {
-                    LOGGER.debug("[DBEvent {}] Request", eventName);
-                    Telemetry.DB_REQUEST_COUNTER.labels(this.getName()).inc();
-                });
-
-        return this.eventCache.getOrCache(eventName, getDBEvent);
-    }*/
-
     public void invalidateCache(ObjectId objectId) {
         if (objectId == null)
             return;
@@ -164,8 +112,8 @@ public class EventsCollection extends DatabaseCollection { //TODO: replace conso
         this.eventCache.remove(objectId);
     }
 
-    public Flux<Object> getDBMember(String eventName, Snowflake... memberIds) {
-        return null;
+//    public Flux<Object> getDBMember(String eventName, Snowflake... memberIds) {
+//        return null;
 //        return this.getDBEvent(eventName)
 //                .flatMapIterable(DBEvent::getMembers)
 //                .collectMap(DBEventMember::getId)
@@ -178,7 +126,7 @@ public class EventsCollection extends DatabaseCollection { //TODO: replace conso
 //                    }
 //                    return members;
 //                });
-    }
+//    }
 
     public List<DBEvent> getAllEvents() {
         List<DBEvent> dbGroups = new ArrayList<>();
