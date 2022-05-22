bb91cdb82ac5c8ed078b7b7d97ea70256fc43a35
LociStar <locistar.got@gmail.com>
2022-05-22 18:04:23 +0200

    added GetInvitedEvents

--- src/main/java/com/locibot/locibot/command/event_commands/EventGroup.java
+++ src/main/java/com/locibot/locibot/command/event_commands/EventGroup.java
@@ -11,7 +11,8 @@ import java.util.List;
 public class EventGroup extends BaseCmdGroup {
     public EventGroup() {
         super(CommandCategory.EVENT, CommandPermission.USER_GUILD, "Event Commands",
-                List.of(new CreateEventCmd(), new DeleteEventCmd(), new ScheduleEventCmd(), new AddUserEventCmd(), new PublishEventCmd(), new TimeZoneEventCmd(), new GetCreatedEvents())
+                List.of(new CreateEventCmd(), new DeleteEventCmd(), new ScheduleEventCmd(), new AddUserEventCmd(),
+                        new PublishEventCmd(), new TimeZoneEventCmd(), new GetCreatedEvents(), new GetInvitedEvents())
         );
     }
 }
