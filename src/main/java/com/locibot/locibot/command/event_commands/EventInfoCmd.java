24ce439dfce09a9438cdf8816a96a9e9cb92bf2d
LociStar <locistar.got@gmail.com>
2022-05-30 16:04:23 +0200

    made message ephemeral

--- src/main/java/com/locibot/locibot/command/event_commands/EventInfoCmd.java
+++ src/main/java/com/locibot/locibot/command/event_commands/EventInfoCmd.java
@@ -6,9 +6,9 @@ import com.locibot.locibot.core.command.CommandPermission;
 import com.locibot.locibot.core.command.Context;
 import com.locibot.locibot.database.DatabaseManager;
 import discord4j.core.object.command.ApplicationCommandOption;
-import discord4j.core.object.entity.Message;
 import discord4j.core.spec.EmbedCreateFields;
 import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.InteractionFollowupCreateSpec;
 import discord4j.rest.util.Color;
 import reactor.core.publisher.Mono;
 
@@ -20,17 +20,20 @@ public class EventInfoCmd extends BaseCmd {
 
     @Override
     public Mono<?> execute(Context context) {
-        return DatabaseManager.getEvents().getInvitedDBEvent(context.getAuthorId(), context.getOptionAsString("name").orElseThrow()).flatMap(dbEvent ->
-                context.getClient().getUserById(dbEvent.getOwner().getUId()).flatMap(owner -> {
+        return DatabaseManager.getEvents().getInvitedDBEvent(context.getAuthorId(), context.getOptionAsString("name").orElseThrow())
+                .switchIfEmpty(context.createFollowupMessageEphemeral("Event not found").then(Mono.empty()))
+                .flatMap(dbEvent -> context.getClient().getUserById(dbEvent.getOwner().getUId()).flatMap(owner -> {
                     Long time = dbEvent.getBean().getScheduledDate();
-                    return context.createFollowupMessage("Loading").flatMap(Message::delete)
-                            .then(context.createMessage(EmbedCreateSpec.builder()
+                    return context.getEvent().deferReply().withEphemeral(true).then(context.createFollowupMessage(InteractionFollowupCreateSpec.builder()
+                            .ephemeral(true)
+                            .addEmbed(EmbedCreateSpec.builder()
                                     .title("Event: " + dbEvent.getEventName())
                                     .description(dbEvent.getEventDescription())
                                     .author(owner.getUsername(), "", owner.getAvatarUrl())
                                     .color(Color.BLUE)
-                                    .addField(EmbedCreateFields.Field.of("Date & Time", time == null ? "---" : ("<t=" + time + ">"), true))
-                                    .build()));
+                                    .addField(EmbedCreateFields.Field.of("Date & Time", time == null ? "---" : ("<t:" + time + ">"), true))
+                                    .build())
+                            .build()));
                 }));
     }
 }
