678c77ecf69a1088fa20f3a337293f2080e1ced9
LociStar <locistar.got@gmail.com>
2022-05-30 17:35:33 +0200

    added cmd annotation

--- src/main/java/com/locibot/locibot/command/info/InfoGroup.java
+++ src/main/java/com/locibot/locibot/command/info/InfoGroup.java
@@ -1,10 +1,12 @@
 package com.locibot.locibot.command.info;
 
 import com.locibot.locibot.core.command.BaseCmdGroup;
+import com.locibot.locibot.core.command.CmdAnnotation;
 import com.locibot.locibot.core.command.CommandCategory;
 
 import java.util.List;
 
+@CmdAnnotation
 public class InfoGroup extends BaseCmdGroup {
 
     public InfoGroup() {
