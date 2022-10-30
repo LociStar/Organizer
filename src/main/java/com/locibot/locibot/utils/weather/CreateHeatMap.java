4503183a8d0b445a39ee229235912cacc9e1ba8c
LociStar <locistar.got@gmail.com>
2022-10-30 16:55:45 +0100

    removed unnecessary import

--- src/main/java/com/locibot/locibot/utils/weather/CreateHeatMap.java
+++ src/main/java/com/locibot/locibot/utils/weather/CreateHeatMap.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
