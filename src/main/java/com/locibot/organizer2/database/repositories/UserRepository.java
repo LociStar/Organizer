db02bbb50c28da6d1c09ce366ee01fdffdb1ef5c
LociStar <locistar.got@gmail.com>
2023-08-28 22:01:47 +0200

    added setZoneId query

--- src/main/java/com/locibot/organizer2/database/repositories/UserRepository.java
+++ src/main/java/com/locibot/organizer2/database/repositories/UserRepository.java
@@ -1,7 +1,20 @@
 package com.locibot.organizer2.database.repositories;
 
 import com.locibot.organizer2.database.tables.User;
+import discord4j.gateway.GatewayObserver;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+import java.time.ZoneId;
 
 public interface UserRepository extends R2dbcRepository<User, Integer> {
+
+    @Query("""
+            INSERT INTO user_ (id, zone_id)
+            VALUES ($1, $2)
+            ON CONFLICT (id) DO UPDATE SET zone_id = $2
+            RETURNING id;""")
+    Mono<Long> setZoneId(long userId, ZoneId zoneId);
+
 }
