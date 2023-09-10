df9f6c263b01be21c844d06837177542ff81a88d
LociStar <locistar.got@gmail.com>
2023-09-10 18:59:51 +0200

    Update SlashCommand interface with permission check
    Added a method in the SlashCommand interface that deals with user permission checks. The 'getPermission' method returns the user's guild permission. This change is crucial to ensure that only users with requisite permissions can execute the commands thereby enhancing system security. Import statements were also adjusted due to package modifications.

--- src/main/java/com/locibot/organizer2/commands/SlashCommand.java
+++ src/main/java/com/locibot/organizer2/commands/SlashCommand.java
@@ -1,16 +1,21 @@
 package com.locibot.organizer2.commands;
 
-import com.locibot.organizer2.core.CommandContext;
+import com.locibot.organizer2.core.command.CommandContext;
+import com.locibot.organizer2.core.command.CommandPermission;
 import reactor.core.publisher.Mono;
 
 /**
  * A simple interface defining our slash command class contract.
- *  a getName() method to provide the case-sensitive name of the command.
- *  and a handle() method which will house all the logic for processing each command.
+ * a getName() method to provide the case-sensitive name of the command.
+ * and a handle() method which will house all the logic for processing each command.
  */
 public interface SlashCommand {
 
     String getName();
+
+    default CommandPermission getPermission() {
+        return CommandPermission.USER_GUILD;
+    }
 
     Mono<?> handle(CommandContext<?> context);
 }
